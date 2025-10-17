e8e5e013fc1df57b9bee2af05021a375849d12e6
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-17 19:04:09 +0400

    for each Iterator

---
+++ src/main/java/org/example/accesmodifier/collectionframework/list/IchFor.java
@@ -0,0 +1,16 @@
+package org.example.accesmodifier.collectionframework.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IchFor {
+    public static void main(String[] args) {
+
+        List<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(3);
+        num.add(5);
+
+        num.removeIf(n -> n.equals(1));
+    }
+}
