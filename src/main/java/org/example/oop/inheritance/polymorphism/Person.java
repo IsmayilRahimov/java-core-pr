6f39256a532e9f3447adaeedf5e73ff164196fe1
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-29 22:05:38 +0400

    Oop encap

---
+++ src/main/java/org/example/oop/inheritance/polymorphism/Person.java
@@ -0,0 +1,17 @@
+package org.example.oop.inheritance.polymorphism;
+
+public class Person {
+    public static void test(int a) {
+
+    }
+
+    public static void test(String a) {
+
+    }
+
+
+    public static void main(String[] args) {
+        test(10);
+        test("B");
+    }
+}
