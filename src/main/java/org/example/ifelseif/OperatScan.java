49f5d8de29c6d4278f59ab6655717b3572a14401
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-31 02:39:05 +0400

    //practice work work work

--- src/main/java/org/example/ifelseif/OperatScan.java
+++ src/main/java/org/example/ifelseif/OperatScan.java
@@ -3,7 +3,7 @@ package org.example.ifelseif;
 public class OperatScan {
     public static void main(String[] args) {
         int eded;
-        eded = 20;
+        eded = 7;
 
 //        if (eded < 20 && eded % 2 == 0) {
 //            System.out.println("Dogru");
@@ -12,7 +12,7 @@ public class OperatScan {
 //        }
         if (eded > 10 || eded % 2 == 0) {
             System.out.println("Dogru");
-        }else {
+        } else {
             System.out.println("Yalnis");
         }
 
