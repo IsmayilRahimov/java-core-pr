6e5d7781e0149a3fdb96139eef00242ba0d4c8f2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-31 16:09:05 +0400

    for do while ,while

--- src/main/java/org/example/fors/StyWhile.java
+++ src/main/java/org/example/fors/StyWhile.java
@@ -8,8 +8,27 @@ public class StyWhile {
         int toplamcem = 0;
 
         for (int i = 0; i < 10; i++) {
-            System.out.print((i+1) + "-ci ededi daxil edin:");
+            System.out.print((i + 1) + "-ci ededi daxil edin:");
             int eded = input.nextInt();
         }
+
+        int reqem = 0;
+        while (reqem < 10) {
+            System.out.println("ededi gotur:");
+            reqem++;
+        }
+
+        int mes = 0;
+
+        do {
+            System.out.println("Ededi cap et");
+            mes++;
+        } while (mes < 10);
+
+//        do {
+//            int k=  0;
+//            System.out.println("Sonsuz dovr:" + k);
+//            k++;
+//        } while (k < 5);
     }
 }
