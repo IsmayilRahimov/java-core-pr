1378f9bbbf0ed667a2b3200d719a0f0c533092aa
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-02 23:55:49 +0400

    practice for me

---
+++ src/main/java/org/example/array/ArrayDizi.java
@@ -0,0 +1,11 @@
+package org.example.array;
+
+public class ArrayDizi {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 8, 9};
+        String[] arrName = {"Samir", "Musa", "Guseyn"};
+        System.out.println(arr[2]);
+        System.out.println(arrName[1]);
+
+    }
+}
