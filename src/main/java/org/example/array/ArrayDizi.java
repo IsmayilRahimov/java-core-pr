9bebd4622f77c4ba4ebd59c3fd3b767615661b70
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-03 16:39:05 +0400

    practice for me

--- src/main/java/org/example/array/ArrayDizi.java
+++ src/main/java/org/example/array/ArrayDizi.java
@@ -1,14 +1,21 @@
 package org.example.array;
 
+import java.util.Arrays;
+
 public class ArrayDizi {
     public static void main(String[] args) {
         int[] arr = {1, 5, 8, 9};
         String[] arrName = {"Samir", "Musa", "Guseyn"};
-        System.out.println(arr[2]);
-        System.out.println(arrName[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arrName[1]);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int i = 0; i < arrName.length; i++) {
+            System.out.println(arrName[i]);
+        }
+        System.out.println(arrName.length);
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);
         }
 
     }
