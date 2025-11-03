fcc337a2512c0162eb1d608992ff5bc08554a2c4
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-03 18:20:17 +0400

    practice for me

---
+++ src/main/java/org/example/array/Maijn.java
@@ -0,0 +1,12 @@
+package org.example.array;
+
+public class Maijn {
+    public static void empty() {
+        empty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        empty();
+    }
+}
