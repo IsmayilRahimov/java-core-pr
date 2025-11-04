a0b46213c622f35530d8d557a580ef2cd565bc69
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-04 18:38:52 +0400

    string methods

---
+++ src/main/java/org/example/methodstring/StringExampleMethods.java
@@ -0,0 +1,20 @@
+package org.example.methodstring;
+
+public class StringExampleMethods {
+
+
+    public static void main(String[] args) {
+        String ad = "Ragimov";
+        System.out.println(ad);
+        System.out.println(ad.contains("Ra"));
+        System.out.println(ad.charAt(4));
+        System.out.println(ad.substring(1, 4));
+        System.out.println(ad.indexOf("i"));
+        System.out.println(ad.replace("Ra", "Sa"));
+
+        String cumle = "Salam Azerbaycan Xalqi";
+        String[] cumleler = cumle.split(" ");
+        System.out.println(cumleler[0]);
+        System.out.println(cumleler[1]);
+    }
+}
