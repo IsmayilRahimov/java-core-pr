6375bf498c13c22ea90f14bffc0c4adf6a64103b
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-05 00:08:42 +0400

    string methods Long integer float

---
+++ src/main/java/org/example/mathmethods/TaNewLong.java
@@ -0,0 +1,4 @@
+package org.example.mathmethods;
+
+public class TaNewLong                                                                                                                                                                                                                                                                                                                                            {
+}
