2de2322452738f4ee564b0d43fff1218ff6f7ab0
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-05 21:21:39 +0400

    access modifiers

--- src/main/java/org/example/accesmodifiers/MainProtect.java
+++ src/main/java/org/example/accesmodifiers/MainProtect.java
@@ -4,5 +4,7 @@ public class MainProtect {
     public static void main(String[] args) {
         ProtectedMethods p2 = new ProtectedMethods();
         p2.add();
+        MainProtect m2 = new MainProtect();
+
     }
 }
