607ab4c37e37a7432aaf941f672aaf7013852dd3
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-05 19:29:23 +0400

    access modifiers

---
+++ src/main/java/org/example/accesmodifiers/MainlesPrivate.java
@@ -0,0 +1,11 @@
+package org.example.accesmodifiers;
+
+public class MainlesPrivate {
+
+    public static void main(String[] args) {
+        PrivateFilds p2 = new PrivateFilds();
+        p2.setName("Faiq");
+        p2.getName();
+
+    }
+}
---
+++ src/main/java/org/example/accesmodifiers/PrivateFilds.java
@@ -0,0 +1,24 @@
+package org.example.accesmodifiers;
+
+import org.example.accesmodifier.ProtectedAccess;
+
+public class PrivateFilds {
+
+    private String name;
+
+    private int size;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        PrivateFilds p2 = new PrivateFilds();
+        p2.name = "Anar";
+        System.out.println(p2.name);
+    }
+}
