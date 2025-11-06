a6eb69b1e43ff3fe2c220c98de0a051f2ed7d841
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-06 18:56:37 +0400

    access modifiers

---
+++ src/main/java/org/example/accesmodifier/MethodsPrivateScanner.java
@@ -0,0 +1,58 @@
+package org.example.accesmodifier;
+
+public class MethodsPrivateScanner {
+
+    private String name;
+    private int age;
+    private String surname;
+    private int salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            System.out.println("Adinizi duzgun daxil edin - Bosh ola bilmez!");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        try {
+            if (age > 100) {
+                throw new RuntimeException("Xeta bas verdi yas 100-den yuxari ola bilmez!");
+            } else {
+                this.age = age;
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary > 5000 && salary < 500) {
+            throw new RuntimeException("Xeta bas verdi .Maas  telebinizi dogru qeyd edin!");
+        }
+        this.salary = salary;
+    }
+
+}
---
+++ src/main/java/org/example/accesmodifier/ScannerInputPrivate.java
@@ -0,0 +1,28 @@
+package org.example.accesmodifier;
+
+import java.util.Scanner;
+
+public class ScannerInputPrivate {
+
+    public static void main(String[] args) {
+        MethodsPrivateScanner m2 = new MethodsPrivateScanner();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Adinizi daxil edin:");
+        String name = input.nextLine();
+        System.out.println("Yasinizi daxil edin:");
+        int age = input.nextInt();
+        System.out.println("Maas gozlentinizi daxil edin:");
+        int salary = input.nextInt();
+        System.out.println("Ve sonda soyadinizi daxil edin:");
+        String surname = input.nextLine();
+
+
+
+
+        m2.setName(name);
+        m2.setAge(age);
+        m2.setSalary(salary);
+        m2.setSurname(surname);
+    }
+
+}
