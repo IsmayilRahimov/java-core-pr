fee4bf4022350bd38f835a63554aee4c67974731
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-06 20:30:40 +0400

    toString

---
+++ src/main/java/org/example/constructors/Students.java
@@ -0,0 +1,46 @@
+package org.example.constructors;
+
+public class Students {
+    private String name;
+    private int age;
+
+    public Students(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public static void main(String[] args) {
+        Students s1 = new Students("Azad", 21);
+        System.out.println(s1);
+
+    }
+}
