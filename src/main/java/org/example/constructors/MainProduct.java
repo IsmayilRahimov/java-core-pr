278ee2a984e7f689aa34d716b794c520d25af8f2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-07 15:26:33 +0400

    composition

--- src/main/java/org/example/constructors/MainProduct.java
+++ src/main/java/org/example/constructors/MainProduct.java
@@ -5,5 +5,9 @@ public class MainProduct {
     public static void main(String[] args) {
         Product product = new Product("Tsh-1", "Gaming NootBook", 2.500, 1, 100);
         ProductUsed productUsed = new ProductUsed(product);
+
+        productUsed.add();
+        productUsed.delete();
+
     }
 }
