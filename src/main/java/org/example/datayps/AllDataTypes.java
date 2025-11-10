621528ec6ebc49d720a21500974dbbb017be6b6c
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-10 18:22:45 +0400

    rentalcar

---
+++ src/main/java/org/example/datayps/AllDataTypes.java
@@ -0,0 +1,25 @@
+package org.example.datayps;
+
+public class AllDataTypes {
+
+    public static void main(String[] args) {
+        int i = 1999999999;
+        System.out.println(i);
+        byte b = 127;  // -128 && 127 arasinda
+        System.out.println(b);
+        short s = 32767;  // -32768 32767
+        System.out.println(s);
+        double d = 19.2;
+        System.out.println(d); // ondaliqli eded
+        long l = 1999999999;
+        System.out.println(l);
+        float f = 55.55f;
+        System.out.println(f);
+        boolean dogrumu = true;
+        System.out.println(dogrumu);
+        char karakter = 'A';
+        System.out.println(karakter);
+        char masin = 'F';
+        System.out.println(masin);
+    }
+}
