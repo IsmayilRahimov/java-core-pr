987df318ae8db9082f40fe8e7125c9c3de26371d
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-11 22:02:45 +0400

    encapsulation private methods

---
+++ src/main/java/org/example/privatemethods/SecretClass.java
@@ -0,0 +1,8 @@
+package org.example.privatemethods;
+
+public class SecretClass {
+
+    private void sleep() {
+        System.out.println("Private method ready!");
+    }
+}
---
+++ src/main/java/org/example/privatemethodtesting/TestPrivate.java
@@ -0,0 +1,11 @@
+package org.example.privatemethodtesting;
+
+import org.example.privatemethods.SecretClass;
+
+public class TestPrivate extends SecretClass {
+
+
+    protected void sleep() {
+        System.out.println();
+    }
+}
