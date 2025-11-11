da8d8ba6e8adf43e2db7b64cf17016438a9acb1d
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-11 23:28:47 +0400

    polymorphism

--- src/main/java/org/example/polymorpsihm/PolymorpishmMain.java
+++ src/main/java/org/example/polymorpsihm/PolymorpishmMain.java
@@ -1,4 +1,12 @@
 package org.example.polymorpsihm;
 
 public class PolymorpishmMain {
+
+    public static void main(String[] args) {
+        Employe employe;
+        employe = new Designer();
+        employe.calculateSalary();
+        employe = new SoftWareDeveloper();
+        employe.calculateSalary();
+    }
 }
