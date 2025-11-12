9a6248b639a9082cb4c9d8d58bf8678d18c158e2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-12 20:05:24 +0400

    intervyu test

--- src/main/java/org/example/modifier/StringcClas.java
+++ src/main/java/org/example/modifier/StringcClas.java
@@ -1,7 +1,17 @@
 package org.example.modifier;
 
+import org.example.polymorpsihm.Payment;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StringcClas {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String s = "Java ";
         s = s.concat("Pograming");
         System.out.println(s);
@@ -19,5 +29,20 @@ public class StringcClas {
         } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db\",\"root");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        File file;
+        try {
+            file = new File("Test44.txt");
+            FileReader fr = new FileReader(file);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
