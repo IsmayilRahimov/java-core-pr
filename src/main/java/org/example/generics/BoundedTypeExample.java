12961233abfe7b498a8c090ffb7f13716c71f6a2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-13 19:13:00 +0400

    generics methods

--- src/main/java/org/example/generics/BoundedTypeExample.java
+++ src/main/java/org/example/generics/BoundedTypeExample.java
@@ -13,10 +13,21 @@ public class BoundedTypeExample {
         System.out.println(karakter);
     }
 
+    public static <M extends Number> void showKharacter(M karakter) {
+        System.out.println(karakter.shortValue());
+    }
+
+    public static void showKharacter(Character karakter) {
+        System.out.println((short) karakter.charValue());
+    }
+
+
     public static void main(String[] args) {
         showDoubleValue(10);
         showDoubleValue(20);
         sim(10.22);
         show('A');
+        showKharacter('A');
+        showKharacter(10);
     }
 }
