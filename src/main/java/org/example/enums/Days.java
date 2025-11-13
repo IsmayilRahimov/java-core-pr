4974ccbc8a3bf9b959332136fc9a4c995da507e8
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-13 17:38:31 +0400

    enums

---
+++ src/main/java/org/example/enums/Days.java
@@ -0,0 +1,18 @@
+package org.example.enums;
+
+public enum Days {
+    BAZARERTESI(1),
+    CERSENBEAXSAMI(2),
+    CERSENBE(3),
+    CUMEAXSAMI(4),
+    CUME(5),
+    SENBE(6),
+    BAZAR(7);
+
+    int index;
+
+    Days(int index) {
+        this.index = index;
+
+    }
+}
---
+++ src/main/java/org/example/enums/MainEnum.java
@@ -0,0 +1,26 @@
+package org.example.enums;
+
+public class MainEnum {
+
+    public static void main(String[] args) {
+        Days day = Days.BAZARERTESI;
+        Days day2 = Days.CERSENBEAXSAMI;
+        Days day3 = Days.CERSENBE;
+        Days day4 = Days.CUMEAXSAMI;
+        Days day5 = Days.CUME;
+        Days day6 = Days.SENBE;
+        Days day7 = Days.BAZAR;
+
+        System.out.println(day.index);
+        System.out.println(day2.index);
+        System.out.println(day2.index);
+        System.out.println(day3.index);
+        System.out.println(day4.index);
+        System.out.println(day5.index);
+        System.out.println(day6.index);
+        System.out.println(day7.index);
+
+
+
+    }
+}
