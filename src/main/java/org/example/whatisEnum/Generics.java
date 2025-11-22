c57e2a7ee6f6867a32d898732c0aae81b9421dac
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-22 22:21:10 +0400

    //

---
+++ src/main/java/org/example/whatisEnum/Generics.java
@@ -0,0 +1,20 @@
+package org.example.whatisEnum;
+
+public class Generics<T> {
+
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Generics<String> generics = new Generics<>();
+        generics.setValue("Aysu");
+        System.out.println(generics.getValue());
+    }
+}
