d6df3fac6a8368e1b08e8cc126a1a31e91d28528
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-11-22 22:26:06 +0400

    enums

---
+++ src/main/java/org/example/whatisEnum/Enums.java
@@ -0,0 +1,19 @@
+package org.example.whatisEnum;
+
+public enum Enums {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+
+    public void showEnum() {
+
+    }
+
+
+}
